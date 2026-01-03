8b5eaec937ce7e8adbe61d0842c31a6ac863870f
spiers448 <Spiers-B3@ulster.ac.uk>
2026-01-03 16:47:01 +0000

    added user authentication system, updated user class

--- QuizSystem/Program.cs
+++ QuizSystem/Program.cs
@@ -1,2 +1,102 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using QuizSystem;
+
+public class Program
+{
+
+
+    // Starting point for the application
+    public static void Main()
+    {
+        List<User> users = new List<User>();
+        users = LoadUsers();
+        LoginSequence(users);
+    }
+
+    public static void LoginSequence(List<User> allUsers)
+    {
+        Console.WriteLine("Enter username:");
+        string nameInput = Console.ReadLine();
+
+        Console.WriteLine("Enter password:");
+        string passwordInput = Console.ReadLine();
+
+        //check if user exists in list
+        User foundUser = allUsers.FirstOrDefault(u => u.UserName == nameInput);
+
+        //if user doesnt exist
+        if (foundUser == null)
+        {
+            Console.WriteLine("Username or password incorrect");
+            return;
+        }
+
+        //authenticate user details
+        bool isSuccessful = foundUser.Login(nameInput, passwordInput);
+
+        if (isSuccessful)
+        {
+            Console.WriteLine("Login successful");
+
+            switch (foundUser.Role)
+            {
+                case UserRole.Admin:
+                    ShowAdminMenu();
+                    break;
+                case UserRole.User:
+                    ShowUserMenu();
+                    break;
+            }
+
+
+        }
+    }
+
+    public static void ShowAdminMenu()
+    {
+        Console.WriteLine("admin");
+    }
+
+    public static void ShowUserMenu()
+    {
+        Console.WriteLine("user");
+    }
+
+
+    public static List<User> LoadUsers()
+    {
+        string filename = "users.csv";
+
+        //does the file exisit within the working dir
+        if (!File.Exists(filename))
+        {
+            Console.WriteLine("File not found");
+            return new List<User>();
+        }
+
+        //LINQ query
+        List<User> _users = File.ReadAllLines(filename)
+            .Skip(1) //skip header row
+            .Where(line => !string.IsNullOrEmpty(line))
+            .Select(line =>
+            {
+                string[] columns = line.Split(','); //split line by comma
+                                                    // role is in column 3
+                UserRole parsedRole = (UserRole)Enum.Parse(typeof(UserRole), columns[3]);
+
+                //default constructor
+                return new User
+                {
+                    UserId = int.Parse(columns[0]),
+                    UserName = columns[1],
+                    Password = columns[2],
+                    Role = parsedRole,
+                    ContactInfo = columns[4]
+                };
+
+            }).ToList();
+        return _users;
+    }
+
+
+
+}
--- QuizSystem/User.cs
+++ QuizSystem/User.cs
@@ -8,52 +8,118 @@ using System.Threading.Tasks;
 
 namespace QuizSystem
 {
+
+    //Enum definition for User Roles
+    public enum UserRole
+    {
+        Admin,
+        User
+    }
+
+
     public class User
     {
-        //private fields
-        private int userID;
-        private string userName;
+        // Private fields
+        private int userId;
+        private string username;
         private string password;
-        private string email;
-        private string role;
+        private UserRole role;
+        private string contactinfo;
+        private bool isLoggedIn;
 
-        //public fields
-        public int UserID
+        // Public properties
+        public int UserId
         {
-            get { return userID; }
-            set { userID = value; }
+            get { return userId; }
+            set { userId = value; }
         }
         public string UserName
         {
-            get { return userName; }
-            set { userName = value; }
+            get { return username; }
+            set { username = value; }
         }
         public string Password
         {
             get { return password; }
             set { password = value; }
         }
-        public string Email
-        {
-            get { return email; }
-            set { email = value; }
-        }
-        public string Role
+        public UserRole Role
         {
             get { return role; }
             set { role = value; }
         }
-
-
-        //constructor
-        public User(int userID, string userName, string password, string email, string role)
+        public string ContactInfo
         {
-            this.userID = userID;
-            this.userName = userName;
-            this.password = password;
-            this.email = email;
-            this.role = role;
+            get { return contactinfo; }
+            set { contactinfo = value; }
+        }
+        public bool IsLoggedIn
+        {
+            get { return isLoggedIn; }
+            set { isLoggedIn = value; }
+        }
 
+        //default constructor
+        public User()
+        {
+
+        }
+
+        //Paramaterised Constructor
+        public User(int userId, string username, string password, string contactInfo, bool IsLoggedIn)
+        {
+            UserId = userId;
+            UserName = username;
+            Password = password;
+            Role = role;
+            ContactInfo = contactinfo;
+            IsLoggedIn = false;
+        }
+
+        // Methods
+        public bool Login(string userName, string password)
+        {
+            if (UserName == userName && Password == password)
+            {
+                // username and password are authenticated.
+                // Advise user that their login is successful.
+                IsLoggedIn = true;
+                Console.WriteLine("Login successful");
+            }
+            else
+            {
+                // username and/or password are incorrect. Advise user
+                // that their login details are incorrect.
+                IsLoggedIn = false;
+                Console.WriteLine($"Login failed. Username {userName} or password is incorrect");
+            }
+            return IsLoggedIn;
+        }
+
+        public void Logout()
+        {
+            if (IsLoggedIn)
+            {
+                IsLoggedIn = false;
+                Console.WriteLine("User is logged out successfully.");
+            }
+            else
+            {
+                Console.WriteLine("User is not logged in");
+            }
+        }
+
+        public void ControlDevice(int deviceId, string deviceName, string deviceAction)
+        {
+            if (isLoggedIn)
+            {
+                Console.WriteLine($"Performing action {deviceAction} on {deviceName}");
+                // Add logic to control the smart device here
+            }
+            else
+            {
+                Console.WriteLine("Please log in to control the device");
+            }
         }
 
     }
